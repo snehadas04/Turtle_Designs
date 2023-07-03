3ba3f838737cd26e3d2b91b0123d5c3b3f655190
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-03 23:56:40 +0530

    Add files via upload

---
+++ design4.py
@@ -0,0 +1,12 @@
+import turtle as t
+import colorsys as cs
+t.bgcolor('black')
+t.width(8)
+t.speed(20)
+t.tracer(30)
+h=0
+
+for i in range(600):
+    c=cs.hsv_to_rgb(h, 1, 1)
+    h+=0.005
+    t.color(c)
