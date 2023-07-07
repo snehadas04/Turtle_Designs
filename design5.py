960c93f358322c62d835924d8de38567fe6f7341
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-07 23:25:21 +0530

    Add files via upload

--- design5.py
+++ design5.py
@@ -1,11 +1,11 @@
 import turtle
 t = turtle.Turtle()
 s = turtle.Screen()
-s.bgcolor("black")
+s.bgcolor("navy")
 t.width(2)
 t.speed(15)
 col = ('white','pink','cyan')
-for i in range(140):
+for i in range(120):
     t.pencolor(col[i%3])
     t.forward(i*4)
     t.right(121)
