fa37b7e14cb6a7bb462f79798011cdbfbe274330
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-13 23:54:00 +0530

    Add files via upload

--- design6.py
+++ design6.py
@@ -8,10 +8,12 @@ setpos(-90,80)
 hideturtle()
 h = 0.0
 
-for i in range(400):
+for i in range(250):
     c = colorsys.hsv_to_rgb(h,1,1)
     pencolor(c)
     fd(200)
     rt(91)
     circle(60)
     h += 0.009
+
+done()
