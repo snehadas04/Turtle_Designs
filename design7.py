555cd3f8265c691c649600e5c4ca27afec7d900e
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-16 23:52:14 +0530

    Add files via upload

--- design7.py
+++ design7.py
@@ -1,2 +1,12 @@
 import turtle
 import colorsys
+
+turtle.bgcolor("black")
+turtle.speed("fastest")
+c=0.0
+turtle.hideturtle()
+
+for i in range(450):
+    colors = colorsys.hsv_to_rgb(c,1,1)
+    turtle.color(colors)
+    c+=0.005
