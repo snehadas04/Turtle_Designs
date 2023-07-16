a0a19be2e9e7a24dedd9072aa55db1c9b2225d2c
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-16 23:54:32 +0530

    Add files via upload

--- design7.py
+++ design7.py
@@ -10,3 +10,9 @@ for i in range(450):
     colors = colorsys.hsv_to_rgb(c,1,1)
     turtle.color(colors)
     c+=0.005
+
+    turtle.forward(i)
+    turtle.backward(i)
+    turtle.left(10)
+    turtle.right(180)
+    turtle.tracer(10)
